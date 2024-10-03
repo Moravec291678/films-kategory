de3e0a4f09fc2a648834d7ea9cf1cd37a7ab86bf
David Moravec <david.moravec2@seznam.cz>
2024-10-03 17:14:56 +0200

    zobrazujeme filmy podle kategorie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import allMovies from "./data"
+import categories from "./categories"
 
-function App() {
+const App = () => {
+
+  const [typeOfMovie, setTypeOfMovie] = useState("komedie")
+
+  const vysledneFilmy = allMovies.filter((oneMovie) => {
+    return oneMovie["category"] === typeOfMovie
+  })
+
+  console.log(vysledneFilmy);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="all-buttons">
+        {
+          categories.map((oneCategory, index) => {
+            return <button className="one-button" key={index} onClick={() => { setTypeOfMovie(oneCategory) }}>{oneCategory}</button>
+          })
+        }
+      </div>
+
+      <div className="all-movies">
+        {
+          vysledneFilmy.map((oneMovie) => {
+            const { id, image, title, age, tags, description } = oneMovie
+
+            return <div key={id} className="one-movie">
+              <img src={image} alt="" />
+              <h2>{title}</h2>
+              <p>{age}</p>
+              <p>{tags}</p>
+              <p>{description}</p>
+            </div>
+          })
+        }
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
